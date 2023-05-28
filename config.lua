6172828bcc4c418d14c4b9ea537c14474ec2e840
Crafted By Little Dragons <elsandkls@kidshideaway.net>
2023-05-27 22:26:13 -0400

    reworking to be full db based, in progress update
    reworking to be full db based, in progress update

--- config.lua
+++ config.lua
@@ -14,6 +14,23 @@ Config.defaultlang = "en_lang"
        -- open stores
 Config.Key = 0x760A9C6F --[G]
 
+Config.PriceMultiplier = 0.11
+Config.StoreType = "farmersmarket" -- flag field in your items database, change with caution
+
+--Webhook Section, description is in translation
+Config.UseWebhook = true -- Use webhook
+
+--Mandatory Webhook Parts
+Config.WebhookTitle = "Farmers Market Transactions"
+Config.Webhook = "https://discord.com/api/webhooks/1110061327612903445/Omw1z6dFZiBPu3RKO_EzIOQsvX4SP0oaaFbFK3pFRi0bxZ7c10CzEOXj7AFj5Soa0bSq"
+
+--Optional Webhook Parts, if not filled will default vorp_core config
+Config.WebhookColor = ""
+Config.WebhookName = ""
+Config.WebhookLogo = ""
+Config.WebhookLogo2 = ""
+Config.WebhookAvatar = ""
+
     --- STORES ---
 
 Config.Stores = {
@@ -24,23 +41,113 @@ Config.Stores = {
 -----------------------------------------------------------------------------
 --------------------------------------Blackwater-----------------------------
 -----------------------------------------------------------------------------
-  
+     BlackwaterFarmersMarket = {
+        blipAllowed = true,
+        BlipName = "Farmers Market",
+        storeName = "Blackwater Farmers Market",
+        PromptName = "Farmers Market",
+        sprite = 819673798,
+        x = -841.49, y = -1346.65, z = 44.18, h = 111.51,
+        distanceOpenStore = 3.0,
+        NpcAllowed = true,
+        NpcModel = "U_M_M_WaLGENERALSTOREOWNER_01",
+        AllowedJobs = {}, -- jobs allowed
+        JobGrade = 0,
+        category = { "Vegetables","Fruits","Grains","Nuts","Roots","Aromatics","Dairy","Protein","Misc" }, -- you need to add the same words to the buyitems and buyitems category you can add new categories as long the items have the category names
+        storeType = {  "Buy","Sell" }, -- choose the storetype if you translate this you must do the same in the client.lua file
+        StoreHoursAllowed = false, -- if you want the stores to use opening and closed hours
+        RandomPrices = false,
+        StoreOpen = 7, -- am
+        StoreClose = 21 -- pm
+    },     
 -----------------------------------------------------------------------------
 --------------------------------------Rhodes---------------------------------
 -----------------------------------------------------------------------------
-   
+    RhodesFarmersMarket = {
+        blipAllowed = true,
+        BlipName = "Farmers Market",
+        storeName = "Rhodes Farmers Market",
+        PromptName = "Farmers Market",
+        sprite = 819673798,
+        x = 1321.44, y = -1296.95, z = 76.25, h= 153.26,
+        distanceOpenStore = 3.0,
+        NpcAllowed = true,
+        NpcModel = "U_M_M_WaLGENERALSTOREOWNER_01",
+        AllowedJobs = {}, -- jobs allowed
+        JobGrade = 0,
+        category = { "Vegetables","Fruits","Grains","Nuts","Roots","Aromatics","Dairy","Protein","Misc" }, -- you need to add the same words to the buyitems and buyitems category you can add new categories as long the items have the category names
+        storeType = {  "Buy","Sell" }, -- choose the storetype if you translate this you must do the same in the client.lua file
+        StoreHoursAllowed = false, -- if you want the stores to use opening and closed hours
+        RandomPrices = false,
+        StoreOpen = 7, -- am
+        StoreClose = 21 -- pm
+    }, 
 -----------------------------------------------------------------------------
 --------------------------------------St-Denis-------------------------------
 -----------------------------------------------------------------------------
-   
+    StDenisFarmersMarket = {
+        blipAllowed = true,
+        BlipName = "Farmers Market",
+        storeName = "St-Denis Farmers Market",
+        PromptName = "Farmers Market",
+        sprite = 819673798,
+        x = 2842.21, y = -1230.91, z = 47.69, h= 132.43,
+        distanceOpenStore = 3.0,
+        NpcAllowed = true,
+        NpcModel = "U_M_M_WaLGENERALSTOREOWNER_01",
+        AllowedJobs = {}, -- jobs allowed
+        JobGrade = 0,
+        category = { "Vegetables","Fruits","Grains","Nuts","Roots","Aromatics","Dairy","Protein","Misc" }, -- you need to add the same words to the buyitems and buyitems category you can add new categories as long the items have the category names
+        storeType = {  "Buy","Sell" }, -- choose the storetype if you translate this you must do the same in the client.lua file
+        StoreHoursAllowed = false, -- if you want the stores to use opening and closed hours
+        RandomPrices = false,
+        StoreOpen = 7, -- am
+        StoreClose = 21 -- pm
+    }, 
 -----------------------------------------------------------------------------
 --------------------------------------Strawberry-----------------------------
 -----------------------------------------------------------------------------
-    
+    StrawbFarmersMarket = {
+        blipAllowed = true,
+        BlipName = "Farmers Market",
+        storeName = "Strawberry Farmers Market",
+        PromptName = "Farmers Market",
+        sprite = 819673798,
+        x = -1777.12, y = -391.94, z = 157.08, h= 330.92,
+        distanceOpenStore = 3.0,
+        NpcAllowed = true,
+        NpcModel = "mp_u_m_m_nat_farmer_01",
+        AllowedJobs = {}, -- jobs allowed
+        JobGrade = 0,
+        category = { "Vegetables","Fruits","Grains","Nuts","Roots","Aromatics","Dairy","Protein","Misc" }, -- you need to add the same words to the buyitems and buyitems category you can add new categories as long the items have the category names
+        storeType = {  "Buy","Sell" }, -- choose the storetype if you translate this you must do the same in the client.lua file
+        StoreHoursAllowed = false, -- if you want the stores to use opening and closed hours
+        RandomPrices = false,
+        StoreOpen = 7, -- am
+        StoreClose = 21 -- pm
+    }, 
 -----------------------------------------------------------------------------
 --------------------------------------Tumbleweed-----------------------------
 -----------------------------------------------------------------------------
-    
+    TumbleFarmersMarket = {
+        blipAllowed = true,
+        BlipName = "Farmers Market",
+        storeName = "Tumbleweed Farmers Market",
+        PromptName = "Farmers Market",
+        sprite = 819673798,
+        x = -5492.46, y = -2941.06, z = -0.46, h= 47.18,
+        distanceOpenStore = 3.0,
+        NpcAllowed = true,
+        NpcModel = "mp_u_m_m_nat_farmer_01",
+        AllowedJobs = {}, -- jobs allowed
+        JobGrade = 0,
+        category = { "Vegetables","Fruits","Grains","Nuts","Roots","Aromatics","Dairy","Protein","Misc" }, -- you need to add the same words to the buyitems and buyitems category you can add new categories as long the items have the category names
+        storeType = {  "Buy","Sell" }, -- choose the storetype if you translate this you must do the same in the client.lua file
+        StoreHoursAllowed = false, -- if you want the stores to use opening and closed hours
+        RandomPrices = false,
+        StoreOpen = 7, -- am
+        StoreClose = 21 -- pm
+    }, 
 -----------------------------------------------------------------------------
 --------------------------------------Valentine------------------------------
 -----------------------------------------------------------------------------
@@ -66,7 +173,25 @@ Config.Stores = {
 -----------------------------------------------------------------------------
 --------------------------------------Vanhorn--------------------------------
 -----------------------------------------------------------------------------
-    
+    VanFarmersMarket = {
+        blipAllowed = true,
+        BlipName = "Farmers Market",
+        storeName = "Vanhorn Farmers Market",
+        PromptName = "Farmers Market",
+        sprite = 819673798,
+        x = 2998.67, y = 567.76, z = 44.48, h = 107.94,
+        distanceOpenStore = 3.0,
+        NpcAllowed = true,
+        NpcModel = "mp_u_m_m_nat_farmer_01",
+        AllowedJobs = {}, -- jobs allowed
+        JobGrade = 0,
+        category = { "Vegetables","Fruits","Grains","Nuts","Roots","Aromatics","Dairy","Protein","Misc" }, -- you need to add the same words to the buyitems and buyitems category you can add new categories as long the items have the category names
+        storeType = {  "Buy","Sell" }, -- choose the storetype if you translate this you must do the same in the client.lua file
+        StoreHoursAllowed = false, -- if you want the stores to use opening and closed hours
+        RandomPrices = false,
+        StoreOpen = 7, -- am
+        StoreClose = 21 -- pm
+    }, 
  
 }
 
@@ -81,15 +206,10 @@ Config.Stores = {
     -- desc = a description of the item
     -- category = where the item will be displayed at
  
-FarmersMarketItems_SELL = {
-    -- Dairy 
-        { itemLabel = "Milk", itemName = "milk", currencyType = "cash", sellprice = 1, randomprice = math.random(30, 55), desc = "Sell Milk", category = "Dairy" },  
-    -- Protein 
-        { itemLabel = "Eggs", itemName = "eggs", currencyType = "cash", sellprice = 1, randomprice = math.random(30, 55), desc = "Sell Eggs", category = "Protein" },
-    -- Misc
-        { itemLabel = "Hop", itemName = "hop", currencyType = "cash", sellprice = 1, randomprice = math.random(30, 55), desc = "Sell Hop", category = "Misc" },
-        { itemLabel = "Indian Tobbaco", itemName = "Indian_Tobbaco", currencyType = "cash", sellprice = 1, randomprice = math.random(30, 55), desc = "Sell Indian Tobbaco", category = "Misc" },
- }
+_Items_SELL = {
+    -- Vegetables
+    --    { itemLabel = "Artichoke", itemName = "artichoke", currencyType = "cash", price = 1, randomprice = math.random(30, 55), desc = "Sell Artichoke", category = "Vegetables" },  
+}
  
 
 -----------------------------------------------------------------------------
@@ -103,31 +223,31 @@ Config.SellItems = {
     -----------------------------------------------------------------------------
     --------------------------------------Blackwater------------------------------
     ----------------------------------------------------------------------------- 
-        BlackwaterFarmersMarket = FarmersMarketItems_SELL, 
+        BlackwaterFarmersMarket = _Items_SELL, 
     -----------------------------------------------------------------------------
     --------------------------------------Rhodes---------------------------------
     -----------------------------------------------------------------------------
-        RhodesFarmersMarket = FarmersMarketItems_SELL,  
+        RhodesFarmersMarket = _Items_SELL,  
     -----------------------------------------------------------------------------
     --------------------------------------St-Denis-------------------------------
     -----------------------------------------------------------------------------  
-        StDenisFarmersMarket = FarmersMarketItems_SELL, 
+        StDenisFarmersMarket = _Items_SELL, 
     -----------------------------------------------------------------------------
     --------------------------------------Strawberry-----------------------------
     -----------------------------------------------------------------------------   
-        StrawbFarmersMarket = FarmersMarketItems_SELL, 
+        StrawbFarmersMarket = _Items_SELL, 
     -----------------------------------------------------------------------------
     --------------------------------------Tumbleweed-----------------------------
     ----------------------------------------------------------------------------- 
-        TumbleFarmersMarket = FarmersMarketItems_SELL, 
+        TumbleFarmersMarket = _Items_SELL, 
     -----------------------------------------------------------------------------
     --------------------------------------Valentine------------------------------
     -----------------------------------------------------------------------------
-         ValFarmersMarket = FarmersMarketItems_SELL, 
+         ValFarmersMarket = _Items_SELL, 
     -----------------------------------------------------------------------------
     --------------------------------------Vanhorn--------------------------------
     -----------------------------------------------------------------------------
-         VanFarmersMarket = FarmersMarketItems_SELL, 
+         VanFarmersMarket = _Items_SELL, 
     -----------------------------------------------------------------------------      
 }
 
@@ -142,15 +262,10 @@ Config.SellItems = {
     -- desc = a description of the item
     -- category = where the item will be displayed at
  
-    FarmersMarketItems_BUY = {
-        -- Dairy 
-            { itemLabel = "Milk", itemName = "milk", currencyType = "cash", buyprice = 2.50, randomprice = math.random(30, 55), desc = "Buy Milk", category = "Dairy" },  
-        -- Protein 
-            { itemLabel = "Eggs", itemName = "eggs", currencyType = "cash", buyprice = 2.50, randomprice = math.random(30, 55), desc = "Buy Eggs", category = "Protein" },
-        -- Misc
-            { itemLabel = "Hop", itemName = "hop", currencyType = "cash", buyprice = 2.50, randomprice = math.random(30, 55), desc = "Buy Hop", category = "Misc" },
-            { itemLabel = "Indian Tobbaco", itemName = "Indian_Tobbaco", currencyType = "cash", buyprice = 2.50, randomprice = math.random(30, 55), desc = "Buy Indian Tobbaco", category = "Misc" },
-   }
+    _Items_BUY = {
+        -- Vegetables
+        --    { itemLabel = "Artichoke", itemName = "artichoke", currencyType = "cash", price = 2.50, randomprice = math.random(30, 55), desc = "Buy Artichoke", category = "Vegetables" },  
+    }
     
 
 -----------------------------------------------------------------------------
@@ -165,31 +280,31 @@ Config.BuyItems = {
     -----------------------------------------------------------------------------
     --------------------------------------Blackwater------------------------------
     ----------------------------------------------------------------------------- 
-        BlackwaterFarmersMarket = FarmersMarketItems_BUY, 
+        BlackwaterFarmersMarket = _Items_BUY, 
     -----------------------------------------------------------------------------
     --------------------------------------Rhodes---------------------------------
     ----------------------------------------------------------------------------- 
-        RhodesFarmersMarket = FarmersMarketItems_BUY, 
+        RhodesFarmersMarket = _Items_BUY, 
     -----------------------------------------------------------------------------
     --------------------------------------St-Denis-------------------------------
     ----------------------------------------------------------------------------- 
-        StDenisFarmersMarket = FarmersMarketItems_BUY, 
+        StDenisFarmersMarket = _Items_BUY, 
     -----------------------------------------------------------------------------
     --------------------------------------Strawberry-----------------------------
     -----------------------------------------------------------------------------  
-        StrawbFarmersMarket = FarmersMarketItems_BUY, 
+        StrawbFarmersMarket = _Items_BUY, 
     -----------------------------------------------------------------------------
     --------------------------------------Tumbleweed-----------------------------
     ----------------------------------------------------------------------------- 
-        TumbleFarmersMarket = FarmersMarketItems_BUY, 
+        TumbleFarmersMarket = _Items_BUY, 
     -----------------------------------------------------------------------------
     --------------------------------------Valentine------------------------------
     ----------------------------------------------------------------------------- 
-        ValFarmersMarket = FarmersMarketItems_BUY, 
+        ValFarmersMarket = _Items_BUY, 
     -----------------------------------------------------------------------------
     --------------------------------------Vanhorn--------------------------------
     ----------------------------------------------------------------------------- 
-        VanFarmersMarket = FarmersMarketItems_BUY, 
+        VanFarmersMarket = _Items_BUY, 
     
     
 }
